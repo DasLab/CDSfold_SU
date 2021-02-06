876932203af0d26f5e9a5fd050034f4426378497
Andy Watkins <andy.watkins2@gmail.com>
2021-02-06 02:15:06 -0500

    Initial code, only minor changes for loop simplifications

---
+++ src/Util.hpp
@@ -0,0 +1,28 @@
+/*
+ * Util.h
+ *
+ *  Created on: 2014/09/22
+ *      Author: kamegai
+ */
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <string>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class Util {
+public:
+	static void baseReplace(string &base, string from, string to){
+		int pos = 0;
+		while((pos = base.find(from,pos)) != string::npos){
+			base.replace(pos,from.length(),to);
+			pos += to.length();
+		}
+	}
+};
+
+#endif /* UTIL_H_ */
