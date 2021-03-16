9954091186d6fedfc76e02df4136803b8b498ec8
Andy Watkins <andy.watkins2@gmail.com>
2021-03-16 18:18:24 -0400

    New tests of reverse mode. Disable random until we can set random seed.

--- test/run_tests.py
+++ test/run_tests.py
@@ -4,7 +4,9 @@ def run_all_tests(fn):
     """
     Conduct simple tests of the CDSfold executable, accumulating one logfile.
     Tests currently hit "regular" mode, bp width limit, codon exclusion,
-    both, and 'reverse' mode.
+    both, and 'random backtrack' mode.
+
+    Just added tests of the 'reverse' mode, which engages in FE maximization.
     """
 
     system('./src/CDSfold example/mev.faa > {}'.format(fn))
@@ -12,7 +14,13 @@ def run_all_tests(fn):
     system('./src/CDSfold -e GGU example/mev.faa >> {}'.format(fn))
     system('./src/CDSfold -w 20 -e GGC example/mev.faa >> {}'.format(fn))
 
-    system('./src/CDSfold -R example/mev.faa >> {}'.format(fn))
+
+    system('./src/CDSfold -r example/mev.faa >> {}'.format(fn))
+    system('./src/CDSfold -r -w 20 example/mev.faa >> {}'.format(fn))
+    system('./src/CDSfold -r -e CCA example/mev.faa >> {}'.format(fn))
+    system('./src/CDSfold -r -w 20 -e CCA example/mev.faa >> {}'.format(fn))
+
+    #system('./src/CDSfold -R example/mev.faa >> {}'.format(fn))
 
 def diff_output():
     with open('test/output/gold_standard_output.txt') as f, open('test/output/new_output.txt') as g:
