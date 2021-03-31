c857e69995bebae9c9861bf8aa4b9b0d699cd903
timsliu <timliu92@gmail.com>
2021-03-31 11:28:05 -0700

    added commenting and formatted readme

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ make
 ```CDSfold [options] seqfile```
 
 seqfile:
+
     amino acid sequence file in fasta format.
 
 options:
@@ -61,23 +62,29 @@ perform a random traceback
 ## Examples:
 The following command finds a CDS with the most stable secondary structure
 among all possible CDSs translated into the amino acid sequence in test.faa.
+
 ```./src/CDSfold ./example/test.faa```
 
 To limit the maximum distance between base-pairs to be 20-bp, please try:
+
 ```./src/CDSfold -w 20 ./example/test.faa```
 
 To avoid some codons (e.g. GUA and GUC), please try:
+
 ```./src/CDSfold -e GUA,GUC ./example/test.faa```
 
 To design a CDS with unstable secondary structure by a heuristic pseudo-MFE
 maximization, please try:
+
 ```./src/CDSfold -r ./example/test.faa```
 
 To design a CDS of which the secondary strucrure from the first codon to 10-th
 codon is unstable, please try:
+
 ```./src/CDSfold -r -f 1 -t 10 ./example/test.faa```
 
 To design a CDS of which the secondary strucrure from the first codon to 10-th
 codon is stable, please try:
+
 ```./src/CDSfold -f 1 -t 10 ./example/test.faa```
 
--- src/fasta.hpp
+++ src/fasta.hpp
@@ -3,20 +3,30 @@
 // #include <fstream>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 #pragma once
 
+/* Class for holding a single sequence
+ *
+ */
 class eachseq {
   public:
     string desc;
     string seq;
 };
 
+/* Class for holding a collection of amino acid sequences in the
+ * FASTA format
+ */
 class fasta {
   public:
-    fasta(const char *fname); //コンストラクタには戻り値
-                              //が要らないという特別ルールがある。
+    
+    /* constructor - pass in file name */ 
+    fasta(const char *fname);
+   
+    /* destructor - TODO this isn't needed since it does nothing */
     ~fasta();
     auto getDesc() -> string { return data[p].desc; }
     auto getSeq() -> string { return data[p].seq; }
@@ -24,17 +34,20 @@ class fasta {
     void initP() { p = 0; }
     void printP();
 
+    /* iterator used to move onto next instance of eachseq in the data vector
+     * input: none
+     * returns: (int) if there is another sequence that in the vector */
     auto next() -> int {
         p++;
         if (p > numSeq) {
-            p = 0;
+            p = 0;              // TODO - this should return bool not int
             return 0;
         }
         return 1;
     }
 
   private:
-    vector<eachseq> data; // いくつ配列があるか分からないので、Vector型とする。
-    int p;                // ポインタ
-    int numSeq;           // 配列数
+    vector<eachseq> data; // vector holding each individual sequence
+    int p;                // index into the list of sequences 
+    int numSeq;           // number of sequences
 };
