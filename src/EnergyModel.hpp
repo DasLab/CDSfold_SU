6446b804a84a8fd10321cc6d03a5d9bd5915e410
timsliu <timliu92@gmail.com>
2021-04-13 11:59:07 -0700

    added accessor functions instead of problem directly accessing energy parameters

--- src/EnergyModel.hpp
+++ src/EnergyModel.hpp
@@ -54,9 +54,16 @@ public:
     /* updateEnergyFoldParams - updates the fold parameters */
     virtual void updateEnergyFoldParams() = 0;
 
+    /* energy functions */
     virtual int TermAU(int const &type) = 0;
     virtual int E_hairpin(int size, int type, int si1, int sj1, const char *string) = 0;
     virtual int E_intloop(int n1, int n2, int type, int type_2, int si1, int sj1, int sp1, int sq1) = 0;
+    
+    /* accessor functions for certain parameters */
+    virtual int getTerminalAU() = 0;
+    virtual int getMLbase() = 0;
+    virtual int getMLintern(unsigned int i) = 0;
+    virtual int getMLclosing() = 0;
 
 };
 
--- src/ViennaEnergyModel.hpp
+++ src/ViennaEnergyModel.hpp
@@ -55,6 +55,7 @@ public:
 };
 
 class ViennaEnergyModel: public EnergyModel {
+
 protected:
     scaledEnergyParams* energyParams_;
 
@@ -63,7 +64,7 @@ public:
     /* constructor - initialize the energy parameters here */
     ViennaEnergyModel() : energyParams_((scaledEnergyParams*) scale_parameters()) {};
   
-    scaledEnergyParams* getEnergyParams () override {
+    scaledEnergyParams* getEnergyParams() override {
         return energyParams_;
     }
     
@@ -74,10 +75,18 @@ public:
     void updateEnergyFoldParams() override {
         update_fold_params();
     }
-    
+   
+    /* energy functions */
     int TermAU(int const &type);
     int E_hairpin(int size, int type, int si1, int sj1, const char *string);
     int E_intloop(int n1, int n2, int type, int type_2, int si1, int sj1, int sp1, int sq1);
+
+    /* accessor functions */
+    int getTerminalAU() override { return energyParams_->TerminalAU;}
+    int getMLbase() override { return energyParams_->MLbase;}
+    int getMLintern(unsigned int i) override { return energyParams_->MLintern[i];}
+    int getMLclosing() override { return energyParams_->MLclosing;}
+
 };
 
 #endif
