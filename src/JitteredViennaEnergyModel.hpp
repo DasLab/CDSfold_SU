76b198748c44b77ff57bc79c582523c5925f2273
timsliu <timliu92@gmail.com>
2021-05-17 14:08:46 -0700

    started jitter energy model

---
+++ src/JitteredViennaEnergyModel.hpp
@@ -0,0 +1,12 @@
+/*
+ * JitteredViennaEnergyModel.hpp
+ * 
+ * Version of ViennaEnergyModel where the energies have some variance
+ */
+
+#include "ViennaEnergyModel.hpp"
+#pragma once
+
+class JitteredViennaEnergyModel: public ViennaEnergyModel {
+
+}
